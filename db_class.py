c027a29b21c508626a8e17052167afa2594d4bbd
main_pc <chistiytoillett@gmail.com>
2019-05-30 20:09:18 +0300

    'pg'

--- db_class.py
+++ db_class.py
@@ -1,9 +1,9 @@
-import sqlite3
+import psycopg2
 
 
 class DB():
     def __init__(self):
-        self.db_conn = sqlite3.connect('db.db')
+        self.db_conn = psycopg2.connect('postgres://mqiofrybtfrpqa:8d2a45cfac3bb1591cc0de19d11f3e50e7cb6cc365cd609757e45c0907648503@ec2-54-228-243-238.eu-west-1.compute.amazonaws.com:5432/d8f5hg48n0uh9e')
     
     def fast_query(self, query, params=()):
         cursor = self.cursor()
--- db_helpers.py
+++ db_helpers.py
@@ -1,4 +1,4 @@
-import sqlite3
+import psycopg2
 from db_class import DB
 
 
@@ -7,34 +7,34 @@ db = DB()
 
 def is_new_user(user_id):
     try:
-        db.fast_query('insert into users(user_id) values(?)', (user_id,))
-    except sqlite3.IntegrityError:
+        db.fast_query('insert into users(user_id) values(%s)', (user_id,))
+    except:
         return False
     else:
         return True
 
 def set_last_msg_id(user_id, msg_id):
-    db.fast_query('update users set last_msg_id=? where user_id=?', (msg_id, user_id))
+    db.fast_query('update users set last_msg_id=%s where user_id=%s', (msg_id, user_id))
 
 def get_last_msg_id(user_id):
-    data = db.select('select last_msg_id from users where user_id=?', (user_id,))
+    data = db.select('select last_msg_id from users where user_id=%s', (user_id,))
     if data:
         return data[0][0]
     else:
         None
 def get_user_case(user_id):
-    data = db.select('select case_id, omnidesk_id from users where user_id=?', (user_id,))
+    data = db.select('select case_id, omnidesk_id from users where user_id=%s', (user_id,))
     if data[0]:
         return data[0]
     else:
         None
     
 def set_user_case(user_id, case_id, omnidesk_id):
-    db.fast_query('update users set case_id=?, omnidesk_id=? where user_id=?', (case_id, omnidesk_id, user_id))
+    db.fast_query('update users set case_id=%s, omnidesk_id=%s where user_id=%s', (case_id, omnidesk_id, user_id))
 
 if __name__ == '__main__':
     pass
-    db.fast_query('drop table users')
-    db.fast_query('create table users (user_id integer unique, case_id integer, omnidesk_id integer, last_msg_id integer)')
-    #db.fast_query('update users set cookie=?', ('''{"_user": "2bb1975c341efd8209a38d37f64fa7d53556f073654d965da5d62d19b9286ac83a%3A2%3A%7Bi%3A0%3Bs%3A5%3A%22_user%22%3Bi%3A1%3Bs%3A50%3A%22%5B62935%2C%22ef9840cb989abefa405bbf9d8c9ae77c%22%2C2592000%5D%22%3B%7D"}''',))
-    print(get_user_case(1))
+    #db.fast_query('drop table users')
+    #db.fast_query('create table users (user_id integer unique, case_id integer, omnidesk_id integer, last_msg_id integer)')
+    #db.fast_query('update users set cookie=%s', ('''{"_user": "2bb1975c341efd8209a38d37f64fa7d53556f073654d965da5d62d19b9286ac83a%3A2%3A%7Bi%3A0%3Bs%3A5%3A%22_user%22%3Bi%3A1%3Bs%3A50%3A%22%5B62935%2C%22ef9840cb989abefa405bbf9d8c9ae77c%22%2C2592000%5D%22%3B%7D"}''',))
+    #print(is_new_user(1))
